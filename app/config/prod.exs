e81343544386cab3f1bd0fe5d595ae47738d8296
im <poson_hemp@yahoo.co.jp>
2021-10-08 06:13:02 +0900

    delete/ call secret_key_base from prod.exs, dev.exs directry

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -13,7 +13,6 @@ config :kokuraex, KokuraexWeb.Endpoint,
   http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "kokura-ex.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
