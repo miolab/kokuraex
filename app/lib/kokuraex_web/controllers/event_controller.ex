85bb0c0b616a22071468b90a3f983019c48898b3
im <poson_hemp@yahoo.co.jp>
2021-11-13 15:45:37 +0900

    (IN PROGRESS) prepare error handling/ HTTPoison.get

--- app/lib/kokuraex_web/controllers/event_controller.ex
+++ app/lib/kokuraex_web/controllers/event_controller.ex
@@ -11,37 +11,67 @@ defmodule KokuraexWeb.EventController do
   end
 
   def get_connpass_events(keyword, count) do
-    "https://connpass.com/api/v1/event/?keyword=#{keyword}&order=2&count=#{count}"
-    |> HTTPoison.get!()
+    res =
+      "https://connpass.com/api/v1/event/?keyword=#{keyword}&order=2&count=#{count}"
+      |> HTTPoison.get()
+  end
+
+  def handle_httpoison_result(res) do
+    case res do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
+      {:ok, %HTTPoison.Response{status_code: 404}} -> :httpoison_notfound
+      {:error, %HTTPoison.Error{reason: reason}} -> :httpoison_error
+      _ -> :httpoison_unknown
+    end
   end
 
   def connpass_events(keyword, count) do
-    # TODO: エラーハンドリング
-    # - get_connpass_events(keyword, count)が、:error の場合
-    # - カラの場合
-    # - 正常系
-    # Map.has_key?()
-
     res =
       get_connpass_events(keyword, count)
-      |> Map.get(:body)
-      |> Jason.decode!()
+      |> handle_httpoison_result()
 
-    events = res["events"]
+    case res do
+      :httpoison_notfound ->
+        default_events_map()
 
-    # TODO: エラー時にカラを返すか、テスト書く（もしくは両方）
-    # Map.has_key?()
+      :httpoison_error ->
+        %{default_events_map() | title: "Fetch Error"}
 
-    events
-    |> Enum.map(
-      &%{
-        :title => &1["title"],
-        :started_at => &1["started_at"],
-        :ended_at => &1["ended_at"],
-        :catch => &1["catch"],
-        :address => &1["address"],
-        :event_url => &1["event_url"]
-      }
-    )
+      :httpoison_unknown ->
+        %{default_events_map() | title: "Unknown Error"}
+
+      _ ->
+        res_decoded = Jason.decode!(res)
+
+        cond do
+          Map.has_key?(res_decoded, "events") ->
+            res_decoded["events"]
+            |> Enum.map(
+              &%{
+                default_events_map()
+                | title: &1["title"],
+                  started_at: &1["started_at"],
+                  ended_at: &1["ended_at"],
+                  catch: &1["catch"],
+                  address: &1["address"],
+                  event_url: &1["event_url"]
+              }
+            )
+
+          true ->
+            %{default_events_map() | title: "Events Empty"}
+        end
+    end
+  end
+
+  def default_events_map() do
+    %{
+      title: "Not Found",
+      started_at: "-",
+      ended_at: "-",
+      catch: "-",
+      address: "-",
+      event_url: "https://kokura-ex.herokuapp.com/"
+    }
   end
 end
