0247c9df83c4707e50aafff5e0e3c37fc73bdf10
im <poson_hemp@yahoo.co.jp>
2024-04-18 06:19:34 +0900

    feat/ get_github_latest_release_information

--- app/lib/kokuraex/services/news_function.ex
+++ app/lib/kokuraex/services/news_function.ex
@@ -20,4 +20,38 @@ defmodule Kokuraex.Services.NewsFunction do
         nil
     end
   end
+
+  @doc """
+  GET the GitHub repository latest release information and return as Map.
+  """
+  @spec get_github_latest_release_information(String.t(), String.t()) :: %{
+          tag_name: String.t(),
+          created_at: String.t(),
+          url: String.t(),
+          body: String.t()
+        }
+  def get_github_latest_release_information(owner, repo) do
+    cached_value = Cache.get_cache("#{owner}_#{repo}")
+
+    github_release_information =
+      case cached_value do
+        nil -> _get_and_cache_github_latest_release_information(owner, repo)
+        _ -> cached_value
+      end
+
+    cond do
+      github_release_information != nil ->
+        information_json = github_release_information |> Jason.decode!()
+
+        %{
+          tag_name: information_json |> Map.get("tag_name"),
+          created_at: information_json |> Map.get("created_at"),
+          url: information_json |> Map.get("html_url"),
+          body: information_json |> Map.get("body")
+        }
+
+      true ->
+        nil
+    end
+  end
 end
