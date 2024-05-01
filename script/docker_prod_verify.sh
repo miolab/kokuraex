bd0a1ca673505a13cfad358244971be8eb0a9bb2
im <poson_hemp@yahoo.co.jp>
2024-05-02 05:39:14 +0900

    fix/ change Dockerfile for prod to more lightweight

--- script/docker_prod_verify.sh
+++ script/docker_prod_verify.sh
@@ -3,6 +3,8 @@ set -a
 source ./.env
 set +a
 
+docker build -t kokuraex_prod -f app/Dockerfile app/
+
 docker run --rm -p 4000:4000 \
 -e SECRET_KEY_BASE="${SECRET_KEY_BASE}" \
 -e GITHUB_API_TOKEN="${GITHUB_API_TOKEN}" \
