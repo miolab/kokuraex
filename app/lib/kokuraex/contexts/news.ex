91275efb176867ea0ef54c83cd1263947d419475
im <poson_hemp@yahoo.co.jp>
2024-05-14 19:02:34 +0900

    fix/ define and apply type construct

--- app/lib/kokuraex/contexts/news.ex
+++ app/lib/kokuraex/contexts/news.ex
@@ -7,21 +7,24 @@ defmodule Kokuraex.Contexts.News do
   alias Kokuraex.Repo
   alias Kokuraex.Schema.PackageNews
 
+  @typedoc """
+  Defines type of `package_news` table columns map.
+  """
+  @type package_news_map :: %{
+          id: non_neg_integer,
+          inserted_at: String.t(),
+          updated_at: String.t(),
+          package_owner: String.t(),
+          package_name: String.t(),
+          package_release_information_platform: String.t()
+        }
+
   @doc """
   Returns a list of `package_news` all records.
+
+  - Result ORDER is by `updated_at` DESC.
   """
-  @spec get_all_items() ::
-          [
-            %{
-              id: non_neg_integer,
-              inserted_at: String.t(),
-              updated_at: String.t(),
-              package_owner: String.t(),
-              package_name: String.t(),
-              package_release_information_platfor: String.t()
-            }
-          ]
-          | []
+  @spec get_all_items() :: [package_news_map()] | []
   def get_all_items() do
     query =
       from(item in PackageNews,
@@ -31,7 +34,7 @@ defmodule Kokuraex.Contexts.News do
           updated_at: item.updated_at,
           package_owner: item.package_owner,
           package_name: item.package_name,
-          package_release_information_platfor: item.package_release_information_platform
+          package_release_information_platform: item.package_release_information_platform
         },
         order_by: [desc: item.updated_at]
       )
@@ -42,14 +45,22 @@ defmodule Kokuraex.Contexts.News do
   @doc """
   Returns a `package_news` record by the package_name.
   """
-  @spec get_item_by_package_name(String.t()) :: map() | nil
+  @spec get_item_by_package_name(String.t()) :: package_news_map() | nil
   def get_item_by_package_name(package_name) do
-    result = Repo.get_by(PackageNews, package_name: package_name)
+    query =
+      from(item in PackageNews,
+        select: %{
+          id: item.id,
+          inserted_at: item.inserted_at,
+          updated_at: item.updated_at,
+          package_owner: item.package_owner,
+          package_name: item.package_name,
+          package_release_information_platform: item.package_release_information_platform
+        },
+        where: item.package_name == ^package_name
+      )
 
-    case result do
-      nil -> nil
-      _ -> result |> Map.from_struct()
-    end
+    Repo.one(query)
   end
 
   @doc """
