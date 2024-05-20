afb9a2a46197d46819ce609b571cd474751ef59c
im <poson_hemp@yahoo.co.jp>
2024-05-21 07:16:11 +0900

    add/ controllers

---
+++ app/lib/kokuraex_web/controllers/about_html.ex
@@ -0,0 +1,5 @@
+defmodule KokuraexWeb.AboutHTML do
+  use KokuraexWeb, :html
+
+  embed_templates("about_html/*")
+end
---
+++ app/lib/kokuraex_web/controllers/news_controller.ex
@@ -0,0 +1,22 @@
+defmodule KokuraexWeb.NewsController do
+  use KokuraexWeb, :controller
+
+  alias Kokuraex.Services.NewsFunction
+
+  def home(conn, _params) do
+    render(
+      conn,
+      :home,
+      layout: false,
+      github_latest_release_information_list: _github_latest_release_information_list()
+    )
+  end
+
+  defp _github_latest_release_information_list() do
+    [
+      elixir: NewsFunction.github_latest_release_information("elixir-lang", "elixir"),
+      phoenix: NewsFunction.hex_latest_release_information("phoenix"),
+      bumblebee: NewsFunction.github_latest_release_information("elixir-nx", "bumblebee")
+    ]
+  end
+end
