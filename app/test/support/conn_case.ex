7c269bf0942728d81f8a56a8958aed2dc02651e5
im <poson_hemp@yahoo.co.jp>
2024-05-26 13:52:59 +0900

    fix/ application and test files

--- app/test/support/conn_case.ex
+++ app/test/support/conn_case.ex
@@ -31,7 +31,8 @@ defmodule KokuraexWeb.ConnCase do
     end
   end
 
-  setup _tags do
+  setup tags do
+    Kokuraex.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
